b2bc643565036b74f94c7fb2d34973fd54551778
sonia <sonia@psone.ca>
2024-11-29 17:56:01 -0700

    workign on who page

--- themes/procast/page-templates/page-home.php
+++ themes/procast/page-templates/page-home.php
@@ -14,12 +14,19 @@ $serviceArgs = [
   'posts_per_page' => -1,
   'orderby' => 'rand',
 ];
+$newsArgs = [
+  'post_type' => 'news',
+  'post_status' => 'publish',
+  'posts_per_page' => -1,
+  // 'order' => 'ASC',
+];
 /****************************************************
  * STORE ALL LOGIC IN CONTEXT AND OUTPUT TO VIEW
  ***************************************************/
 $context = Timber::context();
 $context['field'] = Timber::get_post();
 $context['services'] = Timber::get_posts($serviceArgs);
+$context['news'] = Timber::get_posts($newsArgs);
 $templates = array( 'page-home.twig' );
 Timber::render( $templates, $context );
 
--- themes/procast/static/js/app.js
+++ themes/procast/static/js/app.js
@@ -50,6 +50,7 @@ jQuery(document).ready(function ($) {
     slidesToScroll: 1,
     prevArrow: $('.prevArrow'),
     nextArrow: $('.nextArrow'),
+    // variableWidth: false,
     responsive: [
       {
         breakpoint: 1500,
@@ -67,10 +68,99 @@ jQuery(document).ready(function ($) {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
+          // mobileFirst: true,
         }
       }
     ]
   });
+
+  // frequesnt Questions and Answers
+  $('.faq-answer').hide();
+  $(function() {
+
+    $(".faq-button").click(function() { 
+    
+      // Cancel the siblings
+      $(this)
+        .siblings(".faq-button")
+        .removeClass("is-active")
+        .children(".faq-answer")
+        .slideUp();
+      
+  
+      // Toggle the item
+      $(this)
+        .toggleClass("is-active")
+        .children(".faq-answer")
+        .slideToggle("ease-out");
+    });
+  });
+  // $('.testimonails-slider').slick('refresh');
+   //Testimonials Slider
+   $('.testimonails-slider').slick({
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrow: true,
+    variableWidth: true,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    cssEase: 'ease-in-out',
+    // adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: false,
+          variableWidth: false, // Disable variable width for smaller screens
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          // autoplay: false,
+          // prevArrow: $('.prevTestArrow'),
+          // nextArrow: $('.nextTestArrow'),
+        }
+      },
+    ]
+  });
+
+  $(window).on('resize', function () {
+    $('.testimonails-slider').slick('setPosition');
+  });
+
+ //news lsider
+ $('.news-slider').slick({
+  dots: false,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrow: true,
+  // variableWidth: false,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 2,
+        infinite: false,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
+
 
 
   // //*** Smooth Scroll ***
